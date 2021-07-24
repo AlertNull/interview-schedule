7c2ff3ecd840689dae0c0b5851c46f55f5351b04
phillzou <zoumingyuan@bytedance.com>
2021-07-24 10:45:01 +0800

    feat: extract config

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # 我的 2021 秋招 
 
-汇总自己 2021 秋招经历，整理笔试题以及面经，使用 [Issues](https://github.com/Mayandev/interview-2021/issues) 进行进度管理。
+汇总自己 2021 秋招经历，整理笔试题以及面经，使用 [Issues](https://github.com/mayandev/interview-2021/issues) 进行进度管理。
 ## 已投递
 - [Shopee](https://github.com/Mayandev/interview-2021/issues/7) -- 2021-07-23
 - [猿辅导](https://github.com/Mayandev/interview-2021/issues/5) -- 2021-07-23
--- index.js
+++ index.js
@@ -2,17 +2,19 @@
 const axios = require('axios');
 const fs = require('fs');
 const config = require('./config.json');
+const API = `https://api.github.com/repos/${config.username}/${config.repo}`;
+const RepoUrl = `https://github.com/${config.username}/${config.repo}`;
 const ANCHOR_NUMBER = 5;
 const TOKEN = process.argv.slice(2)[0];
 async function getIssues(params) {
-  const { data } = await axios.get(`${config.api}/issues?access_token=${TOKEN}`, {
+  const { data } = await axios.get(`${API}/issues?access_token=${TOKEN}`, {
     params,
   });
   return data;
 }
 
 async function getLabels() {
-  const { data } = await axios.get(`${config.api}/labels?access_token=${TOKEN}`);
+  const { data } = await axios.get(`${API}/labels?access_token=${TOKEN}`);
   return data;
 }
 
@@ -31,7 +33,7 @@ async function updateReadme() {
   let readme = `
 # 我的 2021 秋招 
 
-汇总自己 2021 秋招经历，整理笔试题以及面经，使用 [Issues](https://github.com/Mayandev/interview-2021/issues) 进行进度管理。
+汇总自己 2021 秋招经历，整理笔试题以及面经，使用 [Issues](${RepoUrl}/issues) 进行进度管理。
 `;
 
   for (let i = 0; i < labels.length; i++) {
