7ba31864911bfab844738902de33d69aaafffd75
mayandev <phillzou@gmail.com>
2021-08-01 10:14:03 +0800

    feat: update initial readme doc

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function updateReadme() {
   let readme = `
 # 我的 2021 秋招 
 
-汇总自己 2021 秋招经历，整理笔试题以及面经，使用 [Issues](${RepoUrl}/issues) 进行进度管理。
+汇总自己 2021 秋招经历，整理笔试题以及面经，使用 [Issues](${RepoUrl}/issues) 进行进度管理，自动同步 [Google Calendar](https://calendar.google.com/) 的面试日程。
 
 ## 面试日程
 
